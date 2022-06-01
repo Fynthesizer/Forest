ecfcf6ec70ba579637923c53f148cfc559e8a86f
Fynthesizer <finn.mitchellanyon@gmail.com>
2022-06-01 17:01:32 +1200

    Added mobile controls

--- src/script.js
+++ src/script.js
@@ -12,16 +12,19 @@ import { SMAAPass } from "three/examples/jsm/postprocessing/SMAAPass.js";
 import { BokehPass } from "three/examples/jsm/postprocessing/BokehPass.js";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 import { renderUI } from "./ui";
+import { VRButton } from "three/examples/jsm/webxr/VRButton.js";
+import { ARButton } from "three/examples/jsm/webxr/ARButton.js";
+import { DeviceOrientationControls } from "three/examples/jsm/controls/DeviceOrientationControls.js";
 
 const maxTrees = 10;
 const lightColour = new THREE.Color("#cfdae4");
 const lightIntensity = 0.8;
 const reverbSettings = { decay: 15, wet: 0.6 };
 
-let scene, camera, renderer;
+let scene, camera, renderer, controller;
 let composer, renderPass, bloomPass, smaaPass, bokehPass;
 let raycaster, pointer, intersection, controls;
-let terrain, field, moon, cursor;
+let terrain, field, mountains, moon, cursor;
 let modelLoader, skyboxLoader;
 let ambientLight, directionalLight;
 let trees;
@@ -36,6 +39,7 @@ let canLock = false;
 let browser = browserDetect();
 let browserCompatible = browser != "safari" && browser != "firefox";
 if (!browserCompatible) setState("error");
+let isMobile = mobileCheck();
 
 export let scale = scales.diatonic;
 export let oscType = oscillators.pulse;
@@ -80,9 +84,12 @@ function init() {
   document.body.appendChild(renderer.domElement);
 
   //Controls
-  controls = new PointerLockControls(camera, renderer.domElement);
-  controls.addEventListener("unlock", onUnlock);
-  //controls.addEventListener("lock", onLock);
+  if (!isMobile) {
+    controls = new PointerLockControls(camera, renderer.domElement);
+    controls.addEventListener("unlock", onUnlock);
+  } else {
+    controls = new DeviceOrientationControls(camera);
+  }
 
   //Effects
   scene.fog = new THREE.Fog("#25386b", 0.25, 900);
@@ -133,6 +140,9 @@ function init() {
     terrain.scale.setScalar(2);
     scene.add(terrain);
     field = terrain.children[0];
+    mountains = terrain.children[1];
+    if (isMobile) terrain.remove(mountains);
+    console.log(terrain);
   });
 
   //Cursor
@@ -167,6 +177,39 @@ function init() {
   scene.add(ambientLight);
   scene.add(directionalLight);
 
+  //Mobile
+  if (isMobile) {
+    //document.body.appendChild(VRButton.createButton(renderer));
+    renderer.xr.enabled = true;
+    renderer.setAnimationLoop(function () {
+      //renderer.render(scene, camera);
+      composer.render();
+      controls.update();
+      if (terrain != null) updateCursor();
+      trees.children.forEach((tree) => {
+        tree.update();
+      });
+    });
+  }
+  //Desktop
+  else {
+    renderer.setAnimationLoop(function () {
+      composer.render();
+      if (terrain != null) updateCursor();
+      trees.children.forEach((tree) => {
+        tree.update();
+      });
+    });
+  }
+
+  //Listeners
+  window.addEventListener("resize", onResize);
+  window.addEventListener("pointerdown", onClick);
+  window.addEventListener("pointermove", onPointerMove);
+  controller = renderer.xr.getController(0);
+  controller.addEventListener("selectstart", onClick);
+  controller.addEventListener("connected", onXRConnected);
+
   trees = new THREE.Group();
   scene.add(trees);
 }
@@ -199,11 +242,16 @@ function onPointerMove(event) {
 function onClick(event) {
   if (state == "title" && canLock) setState("playing");
   else if (state == "playing") {
-    if (event.button == 0 && canPlantTree()) {
+    if ((event.button == 0 || event.type == "selectstart") && canPlantTree()) {
       const tree = new Tree(intersection.point);
       trees.add(tree);
     }
   }
+}
+
+function onXRConnected(event) {
+  setState("playing");
+  console.log(renderer.xr.getSession());
 }
 
 function onUnlock() {
@@ -211,7 +259,7 @@ function onUnlock() {
 }
 
 var animate = function () {
-  requestAnimationFrame(animate);
+  //requestAnimationFrame(animate);
   if (loaded) composer.render();
   //renderer.render(scene, camera);
   if (terrain != null) updateCursor();
@@ -270,7 +318,7 @@ function clearTrees() {
 window.setState = setState;
 function setState(newState) {
   if (newState == "playing" && canLock) {
-    controls.lock();
+    if (!isMobile) controls.lock();
     state = "playing";
     lpf.frequency.rampTo(10000, 0.5);
     anime({
@@ -336,10 +384,7 @@ function setOsc(newOsc) {
 
 if (browserCompatible) {
   init();
-  animate();
-  window.addEventListener("resize", onResize);
-  window.addEventListener("pointerdown", onClick);
-  window.addEventListener("pointermove", onPointerMove);
+  //animate();
 }
 
 //root.render(UI());
@@ -362,4 +407,20 @@ function browserDetect() {
   }
 
   return browserName;
+}
+
+function mobileCheck() {
+  let check = false;
+  (function (a) {
+    if (
+      /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(
+        a
+      ) ||
+      /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(
+        a.substr(0, 4)
+      )
+    )
+      check = true;
+  })(navigator.userAgent || navigator.vendor || window.opera);
+  return check;
 }
